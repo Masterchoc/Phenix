5cbde6a8b3d22051ae71438279efb73b241bb13d
Mel Florance <mel.florance@gmail.com>
2014-06-22 19:39:54 +0200

    Refactoring controller and add custom model method

--- Application/Controllers/BlogController.php
+++ Application/Controllers/BlogController.php
@@ -4,16 +4,12 @@ class BlogController extends AppController
 {
 	public function index()
 	{
-		$this->query->join     = ['blog_categories'];
-		$this->query->order    = 'date.DESC';
-		$this->query->paginate = ['model'=>'/blog?page=*', 'perPage'=>1];
-
-		$articles = $this->Blog->find($this->query);
+		$articles = $this->Blog->getAll();
 
 		$this->set('articles', $articles['result']);
 		$this->set('pages',    $articles['pages']);
-
 		$this->setLayout('blog');
+		
 		$this->render();
 	}
 
