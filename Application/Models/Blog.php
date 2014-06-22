a7702e38da74e7043410510f46a40035bee5246d
Mel Florance <mel.florance@gmail.com>
2014-06-22 19:40:19 +0200

    Add new getAll method for Blog Model

--- Application/Models/Blog.php
+++ Application/Models/Blog.php
@@ -2,7 +2,14 @@
 
 class Blog extends AppModel
 {
-
+	public function getAll()
+	{
+		$this->query = new Object;
+		$this->query->join     = ['blog_categories'];
+		$this->query->order    = 'date.DESC';
+		$this->query->paginate = ['model'=>'/blog?page=*', 'perPage'=>1];
+		return $this->find($this->query);
+	}
 }
 
 ?>
